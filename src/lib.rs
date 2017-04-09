a4857e0793e01e3a3798e9fc5065e8bcfc0d8761
Donald Whyte <donaldwhyte0@gmail.com>
2017-04-09 21:06:09 +0100

    Add top-level rustdocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//! This crate uses UUID4 generated IDs for requests. After installing the
+//! middleware, request IDs can be accessed in a request handler like so:
+//!
+//! ```
+//! fn request_handler(request: &mut Request) -> IronResult<Response> {
+//!     // ...
+//!     let request_id = request.extensions.get::<RequestId>().unwrap();
+//!     // ...
+//! }
+//! ```
+//!
+//! An example service which returns each request ID to the client is shown
+//! below:
+//!
+//! ```
+//! extern crate iron;
+//! extern crate iron_requestid;
+//!
+//! use iron::prelude::*;
+//! use iron::status::Status;
+//! use iron_requestid::{RequestId, RequestIds};
+//!
+//! fn main() {
+//!     let mut chain = Chain::new(return_request_handler);
+//!
+//!     let (request_ids_before, request_ids_after) = RequestIds::new();
+//!     chain.link_before(request_ids_before);
+//!     chain.link_after(request_ids_after);
+//!
+//!     Iron::new(chain).http("127.0.0.1:3000").unwrap();
+//! }
+//!
+//! fn return_request_handler(request: &mut Request) -> IronResult<Response> {
+//!     match request.extensions.get::<RequestId>() {
+//!         Some(id) => Ok(Response::with((
+//!             Status::Ok,
+//!             id.to_string()))),
+//!         None => Ok(Response::with((
+//!             Status::InternalServerError,
+//!             "could not get request ID")))
+//!     }
+//! }
+//! ```
+
 extern crate iron;
 extern crate uuid;
 
