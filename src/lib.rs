b58a43e230a7e7738ed8479b37383d394a694446
Donald Whyte <donaldwhyte0@gmail.com>
2017-04-09 20:38:56 +0100

    Add middleware for generating unique request IDs using UUID4

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
-#[test]
-fn it_works() {
+extern crate iron;
+extern crate uuid;
+
+use iron::IronResult;
+use iron::error::IronError;
+use iron::middleware::{BeforeMiddleware, AfterMiddleware};
+use iron::request::Request;
+use iron::response::Response;
+use iron::typemap::Key;
+use uuid::Uuid;
+
+pub struct RequestId { }
+impl Key for RequestId {
+    type Value = Uuid;
+}
+
+pub struct RequestIds { }
+impl RequestIds {
+    pub fn new() -> (RequestIds, RequestIds) {
+        (RequestIds {}, RequestIds {})
+    }
+}
+
+impl BeforeMiddleware for RequestIds {
+    fn before(&self, request: &mut Request) -> IronResult<()> {
+        request.extensions.insert::<RequestId>(Uuid::new_v4());
+        Ok(())
+    }
+
+    fn catch(&self, request: &mut Request, err: IronError) -> IronResult<()> {
+        request.extensions.remove::<RequestId>();
+        Err(err)
+    }
+}
+
+impl AfterMiddleware for RequestIds {
+    fn after(&self, request: &mut Request, res: Response) -> IronResult<Response> {
+        request.extensions.remove::<RequestId>();
+        Ok(res)
+    }
+
+    fn catch(&self, request: &mut Request, err: IronError) -> IronResult<Response> {
+        request.extensions.remove::<RequestId>();
+        Err(err)
+    }
 }
